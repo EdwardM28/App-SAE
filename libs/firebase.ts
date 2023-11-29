ac03ad78ae5f171dc41bf0ca89bbfd12ab220ef8
EdwardM28 <edwardmosquera2005@gmail.com>
2023-11-29 12:36:41 -0500

    Agrego app

---
+++ libs/firebase.ts
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDC2buhxFXOC9F-I56zBGKDoJ0J9a4qYVc",
+  authDomain: "eshop-set-up-406500.firebaseapp.com",
+  projectId: "eshop-set-up-406500",
+  storageBucket: "eshop-set-up-406500.appspot.com",
+  messagingSenderId: "245285561636",
+  appId: "1:245285561636:web:377b281a59ec6bbd973fb1"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+export default firebaseApp;
